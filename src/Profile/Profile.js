decfdde78b889a76a2168b35e51f70684f3acbac
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2022-04-02 22:04:54 +0300

    added grid layout

---
+++ src/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Grid} from "@mui/material";
+
+const Profile = () => {
+  return (
+    <Grid container>
+      <Grid item xs={4}>1</Grid>
+      <Grid item xs={8}>2</Grid>
+    </Grid>
+  )
+  ;
+};
+
+export default Profile;
