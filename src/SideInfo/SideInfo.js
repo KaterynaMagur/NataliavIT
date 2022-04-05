d4c67a2f4df8c9026910d3fea29a1e97b7c302bc
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2022-04-05 23:15:38 +0300

    moved text data to variables

--- src/SideInfo/SideInfo.js
+++ src/SideInfo/SideInfo.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
+import {Box, List, ListItem, ListItemIcon, ListItemText, Typography} from "@mui/material";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+
+const list = [
+  {
+    text: "address",
+    icon: LocationOnIcon,
+  },
+  {
+    text: "phone",
+    icon: PhoneIcon,
+  },
+  {
+    text: "email",
+    icon: EmailIcon,
+  }
+];
+
+const title = "Contacts";
 
 const SideInfo = () => {
   return (
     <Box sx={{padding: "15px"}}>
       <Typography variant="h6" align={"center"} color={"white"}>
-        Contacts
+        {title}
       </Typography>
+      <List dense sx={{color: "white"}}>
+        {list.map((listItem) => {
+          return <ListItem>
+            {listItem.icon ? <ListItemIcon>
+              <listItem.icon/>
+            </ListItemIcon> : null}
+            <ListItemText primary={listItem.text}/>
+          </ListItem>
+        })}
+      </List>
     </Box>
   );
 };
